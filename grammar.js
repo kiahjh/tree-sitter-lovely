3b933c7d500bffcbf1a39d815d790b9fd0741ca7
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2025-05-05 10:58:41 -0400

    parsing syntax (roughly)

--- grammar.js
+++ grammar.js
@@ -10,8 +10,86 @@
 module.exports = grammar({
   name: "lovely",
 
+  extras: ($) => [$.comment, $._whitespace],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: ($) => "hello",
+    source_file: ($) => nonempty_separated_list(";", $.expression),
+
+    // extras
+    _whitespace: () => /\s+/,
+    comment: () => token(seq(`#`, /.*\n/)),
+
+    expression: ($) =>
+      choice(
+        $.int_literal,
+        $.ident,
+        $.unit,
+        $.prefix_expression,
+        $.infix_expression,
+        $.function_expression,
+        $.function_call,
+        $.variable_declaration,
+      ),
+
+    // expressions:
+    int_literal: () => /\d+/,
+    ident: () => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    unit: () => "unit",
+    prefix_expression: ($) =>
+      choice(
+        prec(10, seq("+", $.expression)),
+        prec(10, seq("-", $.expression)),
+        prec(10, seq("!", $.expression)),
+      ),
+    infix_expression: ($) =>
+      choice(
+        prec.left(5, seq($.expression, "^", $.expression)),
+        prec.left(4, seq($.expression, "*", $.expression)),
+        prec.left(4, seq($.expression, "/", $.expression)),
+        prec.left(3, seq($.expression, "+", $.expression)),
+        prec.left(3, seq($.expression, "-", $.expression)),
+        prec.left(2, seq($.expression, "<", $.expression)),
+        prec.left(2, seq($.expression, ">", $.expression)),
+        prec.left(2, seq($.expression, "<=", $.expression)),
+        prec.left(2, seq($.expression, ">=", $.expression)),
+        prec.left(1, seq($.expression, "==", $.expression)),
+        prec.left(1, seq($.expression, "!=", $.expression)),
+      ),
+    function_expression: ($) =>
+      seq(
+        "fun",
+        "(",
+        separated_list(",", $.function_parameter),
+        ")",
+        optional($.type),
+        "{",
+        nonempty_separated_list(";", $.expression),
+        "}",
+      ),
+    function_call: ($) =>
+      seq(
+        $.ident,
+        "(",
+        separated_list(",", seq(optional(seq($.ident, ":")), $.expression)),
+        ")",
+      ),
+    variable_declaration: ($) =>
+      prec(
+        0,
+        seq($.ident, ":", optional($.type), choice("=", ":"), $.expression),
+      ),
+
+    function_parameter: ($) =>
+      seq(optional("~"), $.ident, optional($.ident), ":", $.type),
+
+    type: ($) => prec(2, $.ident),
   },
 });
+
+function nonempty_separated_list(separator, rule) {
+  return seq(rule, repeat(seq(separator, rule)));
+}
+
+function separated_list(separator, rule) {
+  return optional(seq(rule, repeat(seq(separator, rule))));
+}
