b0ec56d95937dad88ca09ae7c00212949812cf0d
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2025-07-22 16:39:46 -0400

    updated to new syntax

--- grammar.js
+++ grammar.js
@@ -8,12 +8,12 @@
 // @ts-check
 
 module.exports = grammar({
-  name: "lovely",
+  name: `lovely`,
 
   extras: ($) => [$.comment, $._whitespace],
 
   rules: {
-    source_file: ($) => nonempty_separated_list(";", $.expression),
+    source_file: ($) => repeat($.expression),
 
     // extras
     _whitespace: () => /\s+/,
@@ -23,77 +23,71 @@ module.exports = grammar({
       choice(
         $.int_literal,
         $.variable_ident,
-        $.unit,
         $.prefix_expression,
         $.infix_expression,
         $.function_expression,
         $.function_call,
         $.variable_declaration,
+        $.block_expression,
       ),
 
     // expressions:
     int_literal: () => /\d+/,
     variable_ident: () => /[a-zA-Z_][a-zA-Z0-9_]*/,
-    unit: () => "unit",
-    prefix_expression: ($) =>
-      choice(
-        prec(10, seq("+", $.expression)),
-        prec(10, seq("-", $.expression)),
-        prec(10, seq("!", $.expression)),
-      ),
+    prefix_expression: ($) => choice(prec(10, seq(`!`, $.expression))),
+    block_expression: ($) => seq(`{`, repeat($.expression), `}`),
     infix_expression: ($) =>
       choice(
-        prec.left(5, seq($.expression, "^", $.expression)),
-        prec.left(4, seq($.expression, "*", $.expression)),
-        prec.left(4, seq($.expression, "/", $.expression)),
-        prec.left(3, seq($.expression, "+", $.expression)),
-        prec.left(3, seq($.expression, "-", $.expression)),
-        prec.left(2, seq($.expression, "<", $.expression)),
-        prec.left(2, seq($.expression, ">", $.expression)),
-        prec.left(2, seq($.expression, "<=", $.expression)),
-        prec.left(2, seq($.expression, ">=", $.expression)),
-        prec.left(1, seq($.expression, "==", $.expression)),
-        prec.left(1, seq($.expression, "!=", $.expression)),
+        prec.left(5, seq($.expression, `^`, $.expression)),
+        prec.left(4, seq($.expression, `*`, $.expression)),
+        prec.left(4, seq($.expression, `/`, $.expression)),
+        prec.left(3, seq($.expression, `+`, $.expression)),
+        prec.left(3, seq($.expression, `-`, $.expression)),
+        prec.left(2, seq($.expression, `<`, $.expression)),
+        prec.left(2, seq($.expression, `>`, $.expression)),
+        prec.left(2, seq($.expression, `<=`, $.expression)),
+        prec.left(2, seq($.expression, `>=`, $.expression)),
+        prec.left(1, seq($.expression, `==`, $.expression)),
+        prec.left(1, seq($.expression, `!=`, $.expression)),
       ),
     function_expression: ($) =>
       seq(
-        "fun",
-        "(",
-        separated_list(",", $.function_parameter),
-        ")",
-        optional($.type),
-        "{",
-        nonempty_separated_list(";", $.expression),
-        "}",
+        `fun`,
+        `(`,
+        separated_list(`,`, $.function_parameter),
+        `)`,
+        optional(seq(`->`, $.type)),
+        `:`,
+        $.expression,
       ),
     function_call: ($) =>
       seq(
         $.variable_ident,
-        "(",
+        `(`,
         separated_list(
-          ",",
-          seq(optional(seq($.variable_ident, ":")), $.expression),
+          `,`,
+          seq(optional(seq($.variable_ident, `:`)), $.expression),
         ),
-        ")",
+        `)`,
       ),
     variable_declaration: ($) =>
       prec(
         0,
         seq(
           $.variable_ident,
-          ":",
+          `:`,
           optional($.type),
-          choice("=", ":"),
+          choice(`=`, `:`),
           $.expression,
         ),
       ),
 
     function_parameter: ($) =>
       seq(
-        optional("~"),
+        optional(`~`),
         $.variable_ident,
         optional($.variable_ident),
-        ":",
+        `:`,
         $.type,
       ),
 
@@ -101,10 +95,6 @@ module.exports = grammar({
     type_ident: () => /[A-Z_][a-zA-Z0-9_]*/,
   },
 });
-
-function nonempty_separated_list(separator, rule) {
-  return seq(rule, repeat(seq(separator, rule)));
-}
 
 function separated_list(separator, rule) {
   return optional(seq(rule, repeat(seq(separator, rule))));
